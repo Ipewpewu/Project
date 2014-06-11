9f2943dcede59093f478a80d657ca345ccd0210f
Ipewpewu <warner.m22@gmail.com>
2014-06-10 23:51:58 -0500

    Inititial Commit

---
+++ Source/Le_Project/Le_Project.h
@@ -0,0 +1,8 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __LE_PROJECT_H__
+#define __LE_PROJECT_H__
+
+#include "Engine.h"
+
+#endif
---
+++ Source/Le_Project/Le_ProjectGameMode.cpp
@@ -0,0 +1,16 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#include "Le_Project.h"
+#include "Le_ProjectGameMode.h"
+
+ALe_ProjectGameMode::ALe_ProjectGameMode(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FObjectFinder<UClass> PlayerPawnBPClass(TEXT("Class'/Game/Blueprints/MyCharacter.MyCharacter_C'"));
+	if (PlayerPawnBPClass.Object != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Object;
+
+	}
+}
