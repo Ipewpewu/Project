46eccf81a77bc11aafb3e351485f3cebcd6e3c32
Ipewpewu <warner.m22@gmail.com>
2014-06-29 14:02:34 -0500

    Item class

---
+++ Source/Le_Project/Item.cpp
@@ -0,0 +1,13 @@
+
+
+#include "Le_Project.h"
+#include "Item.h"
+
+
+AItem::AItem(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/Le_Project/Item.h
@@ -0,0 +1,18 @@
+
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AItem : public AActor
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
