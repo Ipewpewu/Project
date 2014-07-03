748558b409143cf5b7b52422a4679aa0326ac65a
Ipewpewu <warner.m22@gmail.com>
2014-07-02 20:30:37 -0500

    Item

--- Source/Le_Project/Item.cpp
+++ Source/Le_Project/Item.cpp
@@ -2,12 +2,23 @@
 
 #include "Le_Project.h"
 #include "Item.h"
-
+#include "ParticleDefinitions.h"
 
 AItem::AItem(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
 {
+	Trigger = PCIP.CreateDefaultSubobject<UBoxComponent>(this, "Trigger");
+	Trigger->OnComponentBeginOverlap.AddDynamic(this, &AItem::OnTouch);
+	RootComponent = Trigger;
 
+	Mesh = PCIP.CreateDefaultSubobject<UStaticMeshComponent>(this, "Mesh");
+	Mesh->AttachTo(RootComponent);
+
+	Particle = PCIP.CreateDefaultSubobject<UParticleSystemComponent>(this, "Particle");
+	Particle->AttachTo(RootComponent);
 }
 
+void AItem::OnTouch(AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
 
+}
--- Source/Le_Project/Item.h
+++ Source/Le_Project/Item.h
@@ -13,6 +13,15 @@ class AItem : public AActor
 {
 	GENERATED_UCLASS_BODY()
 
+	UPROPERTY(Category = Trigger, VisibleAnywhere, BlueprintReadOnly)
+	TSubobjectPtr<class UBoxComponent> Trigger;
 	
-	
+	UPROPERTY(Category = Mesh, VisibleAnywhere, BlueprintReadOnly)
+		TSubobjectPtr<class UStaticMeshComponent> Mesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Particle)
+		TSubobjectPtr<UParticleSystemComponent> Particle;
+
+	UFUNCTION()
+		void OnTouch(AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
